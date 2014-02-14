c04dc447ef4e8003e5c4ba7c6561b07f0a704757
aknl3 <ajit.a26@gmail.com>
2014-02-14 15:08:15 -0500

    Added

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/corob/ur10_ws/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/corob/ur10_ws/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/corob/ur10_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/corob/ur10_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/corob/ur10_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/corob/ur10_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/corob/ur10_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/corob/ur10_ws/build"
+export PYTHONPATH="/home/corob/ur10_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/corob/ur10_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/corob/ur10_ws/src:/opt/ros/hydro/share:/opt/ros/hydro/stacks"
